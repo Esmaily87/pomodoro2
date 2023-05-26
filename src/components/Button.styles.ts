c3bf7dbb58ba811722e56751415c18aa45ca698e
Esmaily Negreiros Peixoto <35602424+Esmaily87@users.noreply.github.com>
2023-05-26 17:26:03 -0300

    Second commit

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -17,10 +17,15 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+  
   background-color: ${props => props.theme.primary};
   color: ${props => props.theme.white};
 
- /* ${props => { 
+
+  /*${props => { 
   return css
     `background-color: ${buttonVariants[props.variant]}
     `
